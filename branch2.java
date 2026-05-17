1a6ea7c60fa52816c4cd4ecefa0c98b79f966874
Harvard_shahan <108354735+Harvard-Shahan@users.noreply.github.com>
2026-05-17 20:27:32 +0530

    changing file content

--- branch2.java
+++ branch2.java
@@ -2,8 +2,6 @@ class branch2{
 public static void main(String []args){
 System.out.println("branch2");
   blah blah blah - can the ai find this mistake?
-return branch2.length(
-  
-);
+return branch2.length();
 }
 }
